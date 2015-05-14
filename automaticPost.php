587843d360dd7507a67ba7df7e4b3e43b4feb65b
curogihu <curoas@yahoo.co.jp>
2015-05-13 23:32:59 -0700

    ひとまず完成
    今度は画像のアップロード、本日の新着のみ取り込む。

--- automaticPost.php
+++ automaticPost.php
@@ -1,91 +1,169 @@
 <?php
   
+
   //inctioのライブラリ呼び出し
   include_once('IXR_Library.php');
 
-  //$bits = new IXR_Base64(file_get_contents("tmp.jpg"))
+  require_once 'simplehtmldom/simple_html_dom.php';
+
+  $page_url = "http://suumo.jp/jj/chintai/ichiran/FR301FC001/?ar=030&bs=040&ta=13&sc=13101&sc=13102&sc=13103&sc=13104&sc=13105&sc=13113&sc=13106&sc=13107&sc=13108&sc=13118&sc=13121&sc=13122&sc=13123&sc=13109&sc=13110&sc=13111&sc=13112&sc=13114&sc=13115&sc=13120&sc=13116&sc=13117&sc=13119&cb=0.0&ct=3.0&mb=0&mt=9999999&et=9999999&cn=9999999&shkr1=03&shkr2=03&shkr3=03&shkr4=03&sngz=&po1=09&";
+  $page_data = mb_convert_encoding(file_get_contents($page_url),'UTF-8','auto');
+  $html = str_get_html($page_data);
 /*
   $imgInfo = getimagesize('tmp.jpg');
   $type = $imgInfo['mime'];
   $bits = new IXR_Base64(file_get_contents('tmp.jpg'));
 */
+
+  //$html = file_get_html('./Untitled_100.html');
+
   //example.comは投稿先アドレスに変える
-  $client = new IXR_Client("http://??????????????/xmlrpc.php");
-  $wp_username = "??????????";
-  $wp_password = "??????????";
+  $client = new IXR_Client("http://xxxxxxxxxxxx/xmlrpc.php");
+  $wp_username = "yyyyyyyyyyyyyyy";
+  $wp_password = "zzzzzzzzzzzzzzz";
+  $cnt = 0;
 
   $returnValue = -1;
 
-  $status = $client->query(
-    "wp.newPost", //使うAPIを指定（wp.newPostは、新規投稿）
-    1, // blog ID: 通常は１、マルチサイト時変更
-    $wp_username, // ユーザー名
-    $wp_password, // パスワード
-    array(
-      'post_author' => 1, // 投稿者ID 未設定の場合投稿者名なしになる。
-      'post_status' => 'publish', // 投稿状態
-      'post_title' => 'テスト投稿です。', // タイトル
-      'post_content' => 'テスト投稿本文です。', //　本文
-      //'terms' => array('category' => array(1))　// カテゴリ追加
-      //'terms' => array('category' => '東京23区')　// カテゴリ追加
-    
-      //最初は１からスタート。 未分類 = 1, 東京23区 = 2...
-      'terms' => array('category' => array(2)), // カテゴリ追加
+  foreach($html->find('div[class=property-body js-cassetLink]') as $house){
+/*
+    if($cnt > 5){
+      break;
+    }
 
-      //タグは事前登録不要
-      'terms_names' => array('post_tag' => array('タグ１','タグ２'))
-    )
-  );
+    $cnt++;
+*/
+    $homeNameAnchor = $house->find('h2.property-header-titlle a.js-cassetLinkHref', 0);
+    $homeName = $homeNameAnchor->plaintext;
+    $rentStr = trim($house->find('div.cassette_detail-point', 0)->plaintext);
+    $comment = getCommentBasedOnRent($rentStr) . '<br /> '; 
+    $homeType = $house->find('div.cassette_detail-desc', 0)->plaintext;
+    $homeOtherFee = $house->find('td.cassette_detail-col2', 0)->plaintext;
+    $homeSize = $house->find('td.cassette_detail-col3', 0)->plaintext;
+    $homeCreatedYear = $house->find('td.cassette_detail-col4', 0)->plaintext;
+    $homeAccess = $house->find('div.cassette_note-leftbox', 0)->plaintext;
+    $homeAddress = $house->find('td', 4)->plaintext;
 
-  if(!$status){
-    die('Something went wrong - '.$client->getErrorCode().' : '.$client->getErrorMessage());
+//      echo '名称: ' . $homeNameAnchor . '\n' . 'test';
 
-  } else {
-    $post_id = $client->getResponse(); //返り値は投稿ID
-    $returnValue = $post_id; 
+    $homeContents = '名称: ' . $homeNameAnchor . '<br>' . 
+                    '家賃: ' . $rentStr . '   ' . $comment . '<br>' .
+                    'タイプ: ' . $homeType . '<br>' .
+                    'その他費用: ' . $homeOtherFee . '<br>' .
+                    '広さ: ' . $homeSize . '<br>' .
+                    '築年数: ' . $homeCreatedYear . '<br>' .
+                    'アクセス: ' . $homeAccess . '<br>' . 
+                    '住所: ' . $homeAddress . '<br>';
+
+    $status = $client->query(
+      "wp.newPost", //使うAPIを指定（wp.newPostは、新規投稿）
+      1, // blog ID: 通常は１、マルチサイト時変更
+      $wp_username, // ユーザー名
+      $wp_password, // パスワード
+      array(
+        'post_author' => 1, // 投稿者ID 未設定の場合投稿者名なしになる。
+        'post_status' => 'publish', // 投稿状態
+        'post_title' => $homeName, // タイトル
+        'post_content' => $homeContents, //　本文
+        //'terms' => array('category' => array(1))　// カテゴリ追加
+        //'terms' => array('category' => '東京23区')　// カテゴリ追加
+      
+        //最初は１からスタート。 未分類 = 1, 東京23区 = 2...
+        'terms' => array('category' => array(2)), // カテゴリ追加
+
+        //タグは事前登録不要
+        'terms_names' => array('post_tag' => array(getWardBasedOnAddress($homeAddress)))
+      )
+    );
+
+    if(!$status){
+      die('Something went wrong - '.$client->getErrorCode().' : '.$client->getErrorMessage());
+
+    } else {
+      $post_id = $client->getResponse(); //返り値は投稿ID
+      $returnValue = $post_id; 
+    }
   }
 
-  $imgInfo = getimagesize('tmp.jpg');
-  $type = $imgInfo['mime'];
-  $bits = new IXR_Base64(file_get_contents('tmp.jpg'));
+  echo "finished";
 
-  $status2 = $client->query(
-    "wp.uploadFile",
-    1,
-    $wp_username,
-    $wp_password,
-    array(
-    'name' => 'test.jpg',
-    'type' => $type,
-    'bits' => $bits,
-    'overwrite' => false,
-    'post_id' => $post_id
-    )
-  );
+  function getHouseName($targetHouse){
+    return $targetHouse->find('h2.property-header-titlle a.js-cassetLinkHref', 0)->plaintext;
+  }
 
-  $img = $client->getResponse();
+  function getCommentBasedOnRent($targetRentStr){
+    $rentNum = (double)str_replace("万円", "", $targetRentStr);
+    $returnMessage = '';
 
+    switch (true){
+        case ($rentNum <= 1):
+          $returnMessage = '近年まれにみる安さ！！';
+          break; 
+
+        case ($rentNum <= 2):
+          $returnMessage = '爆安！';
+          break;
+
+        case ($rentNum <= 3):
+          $returnMessage = '手頃な安さ';
+          break; 
+
+        case ($rentNum <= 4):
+          $returnMessage = 'そこそこの安さ';
+          break; 
+
+        case ($rentNum <= 5):
+          $returnMessage = '人並みの値段';
+          break;
+
+        default:
+          $returnMessage = 'なにこれ？高すぎ';
+          break;
+
+    }
+
+    return $returnMessage;
+  }
+
+  function getWardBasedOnAddress($targetAddress){
+    
+    $wardNameArr = Split(",", "港区,新宿区,品川区,目黒区,大田区,世田谷区," .
+                          "渋谷区,中野区,杉並区,練馬区,台東区,墨田区," .
+                          "江東区,荒川区,足立区,葛飾区,江戸川区," .
+                          "千代田区,中央区,文京区,豊島区,北区,板橋区");
+
+    foreach($wardNameArr as $wardName){
+      if(strstr($targetAddress, $wardName)){
+        return $wardName;
+      }
+    }
+
+    return "Undifined";
+  }
+  
+  //東京都福祉保健局による区分
 /*
-<a href="http://city-cheap-rent.xyz/wp-content/uploads/2015/04/test.jpg">
-  <img class="alignnone size-full wp-image-62" 
-        src="http://city-cheap-rent.xyz/wp-content/uploads/2015/04/test.jpg" 
-        alt="test.jpg" 
-        width="280" 
-        height="210" />
-</a>
-*/
+  function getAreaBasedOnAddress($targetAddress){
+    $eastWardNameArr = Split(",", "港区,新宿区,品川区,目黒区,大田区,世田谷区," . 
+                              "渋谷区,中野区,杉並区,練馬区", ",");
+    
+    $westWardNameeArr = Split(",", "台東区,墨田区,江東区,荒川区,足立区,葛飾区," . 
+                              "江戸川区,千代田区,中央区,文京区,豊島区,北区," . 
+                              "板橋区");
 
-  $status3 = $client->query(
-    "wp.editPost",
-    1,
-    $wp_username, 
-    $wp_password, 
-    $post_id,
-    array("post_thumbnail" => $img['id'])
-  );
+    //なぜか東部が機能しない。
+    foreach($eastWardNameArr as $wardName){
+      if(strstr($targetAddress, $wardName)){
+        return "東京東部";
+      }
+    }
 
-  $thumb = $client->getResponse();
+    foreach($westWardNameeArr as $wardName){
+      if(strstr($targetAddress, $wardName)){
+        return "東京南部";
+      }
+    }
 
-  echo $returnValue
-
-?>
+    return "Undefined";
+  }
+  */
