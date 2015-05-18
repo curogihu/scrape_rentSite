c5073861d00f5f5f9c7feb2aaf4a94c06c4fec13
curogihu <curoas@yahoo.co.jp>
2015-05-17 22:46:20 -0700

    画像差し込みは一旦保留
    明日からしばらくScalaに専念

--- automaticPost.php
+++ automaticPost.php
@@ -1,25 +1,18 @@
 <?php
   
   define("LIMIT_HOUSE_IMAGE_NUM", 3);
+  date_default_timezone_set("Asia/Tokyo");
 
   //inctioのライブラリ呼び出し
   include_once('IXR_Library.php');
 
   require_once 'simplehtmldom/simple_html_dom.php';
 
-  date_default_timezone_set("JST");
-
+  
   //インターネットから取り込み
-  //$page_url = "http://suumo.jp/jj/chintai/ichiran/FR301FC001/?ar=030&bs=040&ta=13&sc=13101&sc=13102&sc=13103&sc=13104&sc=13105&sc=13113&sc=13106&sc=13107&sc=13108&sc=13118&sc=13121&sc=13122&sc=13123&sc=13109&sc=13110&sc=13111&sc=13112&sc=13114&sc=13115&sc=13120&sc=13116&sc=13117&sc=13119&cb=0.0&ct=3.0&mb=0&mt=9999999&et=9999999&cn=9999999&shkr1=03&shkr2=03&shkr3=03&shkr4=03&sngz=&po1=09&";
+  //$page_url = "URL";
   //$page_data = mb_convert_encoding(file_get_contents($page_url),'UTF-8','auto');
   //$html = str_get_html($page_data);
-
-  
-/*
-  $imgInfo = getimagesize('tmp.jpg');
-  $type = $imgInfo['mime'];
-  $bits = new IXR_Base64(file_get_contents('tmp.jpg'));
-*/
 
   //ローカルから取り込み
   $html = file_get_html('./Untitled_100.html');
@@ -28,76 +21,14 @@
   $client = new IXR_Client("http://xxxx/xmlrpc.php");
   $wp_username = "yyyy";
   $wp_password = "zzzz";
-  //$cnt = 0;
 
   $returnValue = -1;
 
   foreach($html->find('div[class=property-body js-cassetLink]') as $house){
-/*
-    if($cnt > 5){
-      break;
-    }
 
-    $cnt++;
-*/
-    $homeNameAnchor = $house->find('h2.property-header-titlle a.js-cassetLinkHref', 0);
-    $homeName = $homeNameAnchor->plaintext;
-    $rentStr = trim($house->find('div.cassette_detail-point', 0)->plaintext);
-    $comment = getCommentBasedOnRent($rentStr) . '<br /> '; 
-    $homeType = $house->find('div.cassette_detail-desc', 0)->plaintext;
-    $homeOtherFee = $house->find('td.cassette_detail-col2', 0)->plaintext;
-    $homeSize = $house->find('td.cassette_detail-col3', 0)->plaintext;
-    $homeCreatedYear = $house->find('td.cassette_detail-col4', 0)->plaintext;
-    $homeAccess = $house->find('div.cassette_note-leftbox', 0)->plaintext;
-    $homeAddress = $house->find('td', 4)->plaintext;
+ //   echo getPostHouseContents($house);
 
-//      echo '名称: ' . $homeNameAnchor . '\n' . 'test';
-
-    $homeContents = '詳細のリンク: ' . $homeNameAnchor . '<br>' . 
-                    '家賃: ' . $rentStr . '   ' . $comment . '<br>' .
-                    'タイプ: ' . $homeType . '<br>' .
-                    'その他費用: ' . $homeOtherFee . '<br>' .
-                    '広さ: ' . $homeSize . '<br>' .
-                    '築年数: ' . $homeCreatedYear . '<br>' .
-                    'アクセス: ' . $homeAccess . '<br>' . 
-                    '住所: ' . $homeAddress . '<br>';
-    echo date("H:i:s");
-    echo "<br><br>";
-    echo $homeContents;
-
-    $houseImageCnt = count($house->find('div.cassette-body div.cassette_carrousel-thumblist img.js-noContextMenu'));
-
-    echo $houseImageCnt . "<br>";
-
-    if(LIMIT_HOUSE_IMAGE_NUM > $houseImageNum){
-      $limitHouseImageNum = $houseImageNum;
-
-    }else{
-      $limitHouseImageNum = LIMIT_HOUSE_IMAGE_NUM;
-    }
-
-    $houseI = $house->find('div.cassette-body div.cassette_carrousel-thumblist img.js-noContextMenu');
-
-    //rel取得ok
-    foreach($houseI as $tst){
-      echo $tst->rel . "<br>";
-    }
-
-  //  for($i = 0; $i < $limitHouseImageNum; $i++){
-      //echo $house->find('div.cassette-body div.cassette_carrousel-thumblist img.js-noContextMenu', 0);
- //   }
-/*
-    $homeImages = $house->find('div.cassette-body div.cassette_carrousel-thumblist img.js-noContextMenu');
-
-    $cnt = 0;
-    foreach($homeImages as $homeImage){
-      echo ($cnt + 1) . " " . $homeImage . "<br>";
-      //echo $cnt;
-      $cnt++;
-    }
-*/
-
-/*
+    //wordpressへの投稿
     $status = $client->query(
       "wp.newPost", //使うAPIを指定（wp.newPostは、新規投稿）
       1, // blog ID: 通常は１、マルチサイト時変更
@@ -106,8 +37,8 @@
       array(
         'post_author' => 1, // 投稿者ID 未設定の場合投稿者名なしになる。
         'post_status' => 'draft', // 投稿状態, draft, private, publish, pendingの４つ
-        'post_title' => $homeName, // タイトル
-        'post_content' => $homeContents, //　本文
+        'post_title' => getHouseName($house), // タイトル
+        'post_content' => getPostHouseContents($house), //　本文
         //'terms' => array('category' => array(1))　// カテゴリ追加
         //'terms' => array('category' => '東京23区')　// カテゴリ追加
       
@@ -115,21 +46,108 @@
         'terms' => array('category' => array(2)), // カテゴリ追加
 
         //タグは事前登録不要
-        'terms_names' => array('post_tag' => array(getWardBasedOnAddress($homeAddress)))
+        //'terms_names' => array('post_tag' => array(getWardBasedOnAddress($house)))
       )
     );
 
     if(!$status){
       die('Something went wrong - '.$client->getErrorCode().' : '.$client->getErrorMessage());
+      continue;
 
-    } else {
+    }else{
       $post_id = $client->getResponse(); //返り値は投稿ID
       $returnValue = $post_id; 
     }
-*/
-  }
+/*   
+    $houseImages = $house->find('div.cassette-body div.cassette_carrousel-thumblist img.js-noContextMenu');
+    $houseImageCnt = count($houseImages);
+    
+    // ここからは画像関連
+    if(LIMIT_HOUSE_IMAGE_NUM > $houseImageCnt){
+      $limitHouseImageNum = $houseImageCnt;
 
+    }else{
+      $limitHouseImageNum = LIMIT_HOUSE_IMAGE_NUM;
+    }
+
+    $i = 0;
+
+    foreach($houseImages as $houseImage){
+      $houseImagePath = $houseImage->rel;
+
+      echo $houseImagePath . "<br>";
+
+      //$foundIdx = strrpos($houseImagePath, "/");
+      $houseImageFileName = substr($houseImagePath, strrpos($houseImagePath, "/") + 1);
+      $reserveFilePath = "images/" . $houseImageFileName;
+
+      $imageFile = file_get_contents($houseImagePath);
+      file_put_contents($reserveFilePath, $imageFile);
+
+      $imgInfo = getimagesize($reserveFilePath);
+      $type = $imgInfo['mime'];
+      
+      //echo $type . "<br>";
+
+      // 原因不明で画像パスないときがある
+      if(!empty($type)){
+
+        $bits = new IXR_Base64(file_get_contents($reserveFilePath));
+
+        $status2 = $client->query(
+          "wp.uploadFile",
+          1,
+          $wp_username,
+          $wp_password,
+          array(
+            'name' => $houseImageName,
+            'type' => $type,
+            'bits' => $bits,
+            'overwrite' => false,
+            'post_id' => $post_id
+          )
+        );
+
+
+      }
+
+      $i++;
+
+      if($i >= $limitHouseImageNum){
+        break;
+      }
+    }
+*/
+  } 
+      
   echo "finished";
+
+  function getPostHouseContents($targetHouse){
+    $homeNameAnchor = $targetHouse->find('h2.property-header-titlle a.js-cassetLinkHref', 0);
+    $homeName = $homeNameAnchor->plaintext;
+    $rentStr = trim($targetHouse->find('div.cassette_detail-point', 0)->plaintext);
+    $comment = getCommentBasedOnRent($rentStr) . '<br /> '; 
+    $homeType = $targetHouse->find('div.cassette_detail-desc', 0)->plaintext;
+    $homeOtherFee = $targetHouse->find('td.cassette_detail-col2', 0)->plaintext;
+    $homeSize = $targetHouse->find('td.cassette_detail-col3', 0)->plaintext;
+    $homeCreatedYear = $targetHouse->find('td.cassette_detail-col4', 0)->plaintext;
+    $homeAccess = $targetHouse->find('div.cassette_note-leftbox', 0)->plaintext;
+    $homeAddress = $targetHouse->find('td', 4)->plaintext;
+
+    //phpでMAPの書き方をぐぐる。　→ 関数化
+    $postHouseContents = '詳細のリンク: ' . $homeNameAnchor . '<br>' . 
+                    '家賃: ' . $rentStr . '   ' . $comment . '<br>' .
+                    'タイプ: ' . $homeType . '<br>' .
+                    'その他費用: ' . $homeOtherFee . '<br>' .
+                    '広さ: ' . $homeSize . '<br>' .
+                    '築年数: ' . $homeCreatedYear . '<br>' .
+                    'アクセス: ' . $homeAccess . '<br>' . 
+                    '住所: ' . $homeAddress . '<br>' .
+                    '投稿日時: ' . date("H:i:s") . '<br><br>';
+
+    return $postHouseContents;
+
+  }
 
   function getHouseName($targetHouse){
     return $targetHouse->find('h2.property-header-titlle a.js-cassetLinkHref', 0)->plaintext;
@@ -169,7 +187,8 @@
     return $returnMessage;
   }
 
-  function getWardBasedOnAddress($targetAddress){
+  function getWardBasedOnAddress($targetHouse){
+    $targetAddress = $targetHouse->find('td', 4)->plaintext;
     
     $wardNameArr = Split(",", "港区,新宿区,品川区,目黒区,大田区,世田谷区," .
                           "渋谷区,中野区,杉並区,練馬区,台東区,墨田区," .
